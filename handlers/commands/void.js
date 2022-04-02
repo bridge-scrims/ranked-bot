6b73aa92ae5035a7c78d970e1616263b1b12acaa
Eltik <xp10d363@gmail.com>
2022-04-02 07:40:27 -0700

    Updates.

---
+++ handlers/commands/void.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+const gameFunctions = require("../../handlers/game/gameFunctions.js");
+const functions = require("../functions.js");
+const channels = require("../../config/channels.json");
+const roles = require("../../config/roles.json");
+
+module.exports.run = async (interaction) => {
+    const errorEmbed = new Discord.EmbedBuilder()
+        .setColor('#a84040')
+        .setDescription("This command isn't finished! Sorry.")
+        .setTimestamp();
+    interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+};
