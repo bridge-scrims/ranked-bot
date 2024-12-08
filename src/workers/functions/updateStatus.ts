7128aa52b0de2ed34aa1a98b7fa1d75919c52fd8
aMeow <152869470+aMeow69@users.noreply.github.com>
2024-12-08 23:08:07 +0100

    Channel status instead of nickname

--- src/workers/functions/updateStatus.ts
+++ src/workers/functions/updateStatus.ts
@@ -1,12 +1,11 @@
 import type { Queue } from "@/database"
 import { getQueueCount } from "@/lib/queue"
-import { Routes } from "discord.js"
 import { getWorker } from ".."
 
 export async function updateStatus(queue: Queue) {
     const client = getWorker(queue)
     if (client?.guilds.cache.has(queue.guildId)) {
-        const body = { nick: `@Players: ${getQueueCount(queue)}` }
-        await client.rest.patch(Routes.guildMember(queue.guildId, "@me"), { body })
+        const body = { status: `Queued Players: ${getQueueCount(queue)}` }
+        await client.rest.put(`/channels/${queue._id}/voice-status`, { body })
     }
 }
